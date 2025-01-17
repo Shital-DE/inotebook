46aa601e9167fea7eb4999ffa552d418f8f2bd88
Shital-DE <dattaentind@gmail.com>
2025-01-17 11:08:50 +0530

    CRUD API Calls.

--- src/components/NoteItem.js
+++ src/components/NoteItem.js
@@ -1,14 +1,19 @@
-import React from "react";
+import React, { useContext } from "react";
+import NoteContext from "../context/notes/NoteContext";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPen, faTrashCan } from "@fortawesome/free-solid-svg-icons";
 
 function NoteItem(props) {
   const { note } = props;
+  const context = useContext(NoteContext);
+  const { deleteNote } = context;
+
   return (
     <div className="col-md-3">
       <div className="card my-3">
         <div className="card-body">
           <h5 className="card-title">{note.title}</h5>
+          <h6 className="card-text">{note.tag}</h6>
           <p className="card-text">{note.description}</p>
           <button
             onClick={() => console.log("Edit clicked")}
@@ -18,7 +23,7 @@ function NoteItem(props) {
             <FontAwesomeIcon icon={faPen} />
           </button>
           <button
-            onClick={() => console.log("Delete clicked")}
+            onClick={() => deleteNote(note._id)}
             className="icon-button delete"
             title="Delete"
           >
--- src/context/notes/NotesState.js
+++ src/context/notes/NotesState.js
@@ -2,6 +2,10 @@ import { useState } from "react";
 import NoteContext from "./NoteContext";
 
 const NoteState = (props) => {
+  const host = "http://localhost:5500/api/";
+  const token =
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjc4MzUzNzdhNDkyZDY4MDJjYmMxN2RlIn0sImlhdCI6MTczNjY2MTI5NX0.RcxgXFLERekBlkFrywYhPULDgXHP8hdwBCPN33JAfUI";
+
   const notesInitial = [
     {
       _id: "6785face7e240f00f1b73c591",
@@ -51,8 +55,64 @@ const NoteState = (props) => {
   ];
 
   const [notes, setNotes] = useState(notesInitial);
+
+  // Add a note
+  const addNote = async (title, description, tag) => {
+    const url = `${host}/notes/add-note`;
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "auth-token": token,
+      },
+      body: JSON.stringify({ title, description, tag }),
+    });
+    const result = response.json();
+    // const note = {
+    //   _id: title,
+    //   user: "67835377a492d6802cbc17de",
+    //   title: title,
+    //   description: description,
+    //   tag: tag,
+    //   date: "2025-01-14T05:49:02.412Z",
+    //   __v: 0,
+    // };
+    setNotes(notes.concat({ title, description, tag }));
+  };
+
+  // Update a note
+  const updateNote = async (id, title, description, tag) => {
+    const url = `${host}/notes/update-note/${id}`;
+    const response = await fetch(url, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+        "auth-token": token,
+      },
+      body: JSON.stringify({ title, description, tag }),
+    });
+    const result = response.json();
+
+    // for (let index = 0; index < notes.length; index++) {
+    //   const element = notes[index];
+    //   if (element._id === id) {
+    //     element.title = title;
+    //     element.description = description;
+    //     element.tag = tag;
+    //   }
+    // }
+  };
+
+  // Delete a note
+  const deleteNote = (id) => {
+    const newNotes = notes.filter((note) => {
+      return note._id !== id;
+    });
+    setNotes(newNotes);
+  };
+
   return (
-    <NoteContext.Provider value={{ notes, setNotes }}>
+    <NoteContext.Provider value={{ notes, addNote, updateNote, deleteNote }}>
       {props.children}
     </NoteContext.Provider>
   );
